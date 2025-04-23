9ec26b871b732eb8a048d1d5e879d477cdbf0277
Seventty <zeroseventty@gmail.com>
2025-04-22 22:00:00 -0400

    Implement HomeScreen with bottom navigation and integrate into main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_app/presentation/screens/home_screen.dart';
 import 'package:weather_app/repositories/weather_repository.dart';
 
 Future<void> main() async {
@@ -15,38 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Weather app',
       debugShowCheckedModeBanner: false,
-      home: WeatherScreen()
-    );
-  }
-}
-
-
-class WeatherScreen extends StatefulWidget {
-  const WeatherScreen({super.key});
-
-  @override
-  State<WeatherScreen> createState() => _WeatherScreenState();
-}
-
-class _WeatherScreenState extends State<WeatherScreen> {
-  final String _temperatureInfo = 'Cargando...';
-
-  @override
-  void initState() {
-    super.initState();
-    cargarClima();
-  }
-
-  void cargarClima() async {
-    final weatherRepo = WeatherRepository();
-    final response = await weatherRepo.fetchWeatherByCurrentLocation();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Weather app')),
-      body: Center(child: Text(_temperatureInfo)),
+      home: HomeScreen()
     );
   }
 }
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentPageIndex = 0;
+
+  final List<NavigationDestination> _destinations = const [
+    NavigationDestination(
+      icon: Icon(Icons.home_outlined),
+      selectedIcon: Icon(Icons.home),
+      label: '',
+    ),
+
+    NavigationDestination(
+      icon: Icon(Icons.search_outlined),
+      selectedIcon: Icon(Icons.search),
+      label: '',
+    ),
+
+    NavigationDestination(
+      icon: Icon(Icons.wb_sunny_outlined),
+      selectedIcon: Icon(Icons.wb_sunny),
+      label: '',
+    ),
+
+    NavigationDestination(
+      icon: Icon(Icons.settings_outlined),
+      selectedIcon: Icon(Icons.settings),
+      label: '',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Weather app')),
+      bottomNavigationBar: NavigationBar(
+        destinations: _destinations,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        selectedIndex: _currentPageIndex,
+        indicatorColor: Colors.transparent,
+        onDestinationSelected: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
